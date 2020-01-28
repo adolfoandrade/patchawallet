fc2ccba54e3277e6dbf4844193b85b89ab2ec2eb
Adolfo Andrade <adolfojosedeandrade@gmail.com>
2020-01-28 11:25:45 -0300

    HealthCheck Security Api

--- Backend/Microservices/Security/src/Patcha.Security/Startup.cs
+++ Backend/Microservices/Security/src/Patcha.Security/Startup.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Logging;
+using Newtonsoft.Json;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace Patcha.Security
@@ -32,27 +36,7 @@ namespace Patcha.Security
 
         private IConfiguration Configuration { get; }
 
-        private IHostingEnvironment Environment { get; }
-
-        public void Configure(IApplicationBuilder application)
-        {
-            if (Environment.IsDevelopment())
-            {
-                application.UseDeveloperExceptionPage();
-                application.UseDatabaseErrorPage();
-            }
-
-            application.UseIdentityServer();
-            application.UseStaticFiles();
-            application.UseMvcWithDefaultRoute();
-            application.UseAuthentication();
-            
-            application.UseSwagger();
-            application.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "PATCHAWALLET SECURITY API V1");
-            });
-        }
+        private IHostingEnvironment Environment { get; }   
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -81,10 +65,49 @@ namespace Patcha.Security
                 .AddAspNetIdentity<ApplicationUser>()
                 .AddDeveloperSigningCredential();
 
+            services.AddHealthChecks();
+
             services.AddSwaggerGen(options =>
             {
                 options.SwaggerDoc("v1", new Info { Title = "PATCHAWALLET SECURITY API", Version = "v1" });
             });
+        }
+
+        public void Configure(IApplicationBuilder application)
+        {
+            if (Environment.IsDevelopment())
+            {
+                application.UseDeveloperExceptionPage();
+                application.UseDatabaseErrorPage();
+            }
+
+            application.UseIdentityServer();
+            application.UseStaticFiles();
+            application.UseMvcWithDefaultRoute();
+            application.UseAuthentication();
+            application.UseSwagger();
+
+            application.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "PATCHAWALLET SECURITY API V1");
+            });
+
+            application.UseHealthChecks("/hc",
+              new HealthCheckOptions
+              {
+                  ResponseWriter = async (context, report) =>
+                  {
+                      var result = JsonConvert.SerializeObject(
+                          new
+                          {
+                              environment = Environment.EnvironmentName,
+                              status = report.Status.ToString(),
+                              services = report.Entries.Select(e => new { key = e.Key, value = Enum.GetName(typeof(HealthStatus), e.Value.Status) })
+                          });
+                      context.Response.ContentType = MediaTypeNames.Application.Json;
+                      await context.Response.WriteAsync(result);
+                  }
+              });
         }
 
     }
